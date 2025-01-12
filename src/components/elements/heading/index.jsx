9d696d1153b01281d6e70a4ef3e22bba85a61d9b
fanialfi <fanialfirdaus@gmail.com>
2025-01-12 08:29:45 +0700

    refactor(headingElement): change to dinamis className

--- src/components/elements/heading/index.jsx
+++ src/components/elements/heading/index.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-export default function HeadingElement({ level, children }) {
+export default function HeadingElement({ level, children, className }) {
   const Tag = `h${level}`;
 
-  return <Tag className="text-center">{children}</Tag>;
+  return <Tag className={className}>{children}</Tag>;
 }
 
 HeadingElement.propTypes = {
   level: PropTypes.number,
   children: PropTypes.node,
+  className: PropTypes.string,
 };
